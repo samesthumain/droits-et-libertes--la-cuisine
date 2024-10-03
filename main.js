49f292d47a0199ede1bcd09b243838831c3069a1
Samuel Desmeules-Voyer <samueldv47@gmail.com>
2024-10-03 05:49:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ let hotdogRetour = document.querySelector(".hotdog-retour")
 let menu = document.querySelector(".nav-menu");
 
 hotdog.addEventListener("click", function(){
-    menu.classList.remove("display-none")
-    menu.classList.add("display-grid")
-    //menu.setAttribute("style", "display: grid");
+    //menu.classList.remove("display-none")
+    //menu.classList.add("display-grid")
+    menu.setAttribute("style", "display: grid");
     hotdog.setAttribute("style", "display: none");
     hotdogRetour.setAttribute("style", "display: block");
 })
