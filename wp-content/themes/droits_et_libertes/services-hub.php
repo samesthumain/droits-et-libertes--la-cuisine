39f2d6473c0aae66838185155025a1ddaff1a808
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2024-12-10 13:20:33 -0500

    test

--- wp-content/themes/droits_et_libertes/services-hub.php
+++ wp-content/themes/droits_et_libertes/services-hub.php
@@ -5,19 +5,16 @@
  */
 
 get_header(); // Affiche header.php
-?>
-<h1><?php  the_title(); ?></h1>
 
-<?php
-if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
-	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
-	$service = new WP_Query('post_type=service');
-	while ( $service -> have_posts() ) : $service -> the_post(); 
 ?>
 
-
-
-<div class="services-container">
+		
+<div class="divider">
+  <img class="divider__losange" src="<?php bloginfo('template_url'); ?>/images/losange_divider.png">
+  <div class="divider__line"></div>
+</div>
+		
+	<div class="services-container">
 
       <div class="img-feuille">
         <!--img src="./sources/medias/image/image_feuille.jpg" alt=""-->
@@ -32,20 +29,23 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
         culturels) reconnus par la Charte internationale des droits de l’homme.
         <img class="arrow-down" />
       </p>
-
-	  
-
-    <div class="liste-luttes">
-		<div class="luttes-box">
-          <a href="./service.html"><?php echo the_field("titre_du_service"); ?></a
-          ><img src="./sources/medias/image/fleche_drop_shadow.png" />
-        </div>
-
-	</div>
-      
+		
+		<div class="liste-luttes">
+<?php $service = new WP_Query(['post_type' => 'service', 'posts_per_page' => -1,]);
+    while ($service->have_posts()):
+        $service->the_post(); ?>
+<div class="luttes-box">
+<a href="./service.html"><?php get_field('titre_du_service'); ?></a
+><img src="./sources/medias/image/fleche_drop_shadow.png" />
 </div>
+<?php endwhile ?>
+</div>
+		
+   
+      </div>
+		
+	   <?php endwhile; // Fermeture de la boucle
 
-<?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
 	get_template_part( 'partials/404' ); // Affiche partials/404.php
